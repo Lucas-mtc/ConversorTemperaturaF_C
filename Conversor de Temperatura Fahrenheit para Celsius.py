b36ab309cc1266f10be82ee34ad39a9f8296fed8
Lucas Moraes Teixeira de Carvalho <lucas.teixeira.carvalho@hotmail.com.br>
2021-10-08 21:44:44 -0300

    Adicionar arquivos de projeto.

---
+++ Conversor de Temperatura Fahrenheit para Celsius.py
@@ -0,0 +1,5 @@
+temperaturaFahrenreit = input("Digite uma temperatura em Fahrenheit: ")
+
+temperaturaCelsius = (float (temperaturaFahrenreit) - 32) * 5 / 9
+
+print("A Temperatura em Celsius é ", temperaturaCelsius)
